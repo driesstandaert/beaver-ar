cba6ce878c3d44afa43d0bf9c3ac9fb8e630d510
driesstandaert <driesstandaert@gmail.com>
2020-08-10 17:27:42 +0100

    Restructure files

--- location/scripts.js
+++ location/scripts.js
@@ -26,21 +26,21 @@ function staticLoadPlaces() {
 
 var models = [
   {
-    url: './assets/beaver/beaver.gltf',
+    url: '../assets/beaver/beaver.gltf',
     scale: '0.02 0.02 0.02',
     rotation: '0 250 0',
     position: '3 -1 0',
     info: 'Beaver',
   },
   {
-    url: './assets/trout/scene.gltf',
+    url: '../assets/trout/scene.gltf',
     scale: '0.5 0.5 0.5',
     rotation: '0 180 0',
     position: '2 -1 0',
     info: 'Trout',
   },
   {
-      url: './assets/magnemite/scene.gltf',
+      url: '../assets/magnemite/scene.gltf',
       scale: '0.4 0.4 0.4',
       info: 'Look up',
       rotation: '0 180 0',
