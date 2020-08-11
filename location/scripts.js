f75384cef743e22cbc279835e63063ccdc15f578
driesstandaert <driesstandaert@gmail.com>
2020-08-11 11:24:30 +0100

    Change location

--- location/scripts.js
+++ location/scripts.js
@@ -30,10 +30,10 @@ function renderPlaces(places) {
 
      let model = document.createElement('a-entity');
      model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-     model.setAttribute('gltf-model', '../assets/magnemite/scene.gltf');
+     model.setAttribute('gltf-model', '../assets/trout/scene.gltf');
      model.setAttribute('rotation', '0 180 0');
      model.setAttribute('animation-mixer', '');
-     model.setAttribute('scale', '0.5 0.5 0.5');
+     model.setAttribute('scale', '1 1 1');
 
      model.addEventListener('loaded', () => {
          window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
