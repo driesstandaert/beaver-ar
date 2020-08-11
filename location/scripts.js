4f64c9e8f1a79e1645b486b0479faf98b2cc15c1
driesstandaert <driesstandaert@gmail.com>
2020-08-11 11:27:47 +0100

    Change location

--- location/scripts.js
+++ location/scripts.js
@@ -15,6 +15,13 @@ function staticLoadPlaces() {
             lng: -0.177321,
          }
      },
+     {
+      name: 'trout',
+      location: {
+         lat: 51.6220518,
+         lng: -0.1779886,
+      }
+    },
  ];
 }
 
@@ -24,13 +31,15 @@ function renderPlaces(places) {
  places.forEach((place) => {
      let latitude = place.location.lat;
      let longitude = place.location.lng;
+     let testje = place.name;
+     console.log(testje);
      console.log(latitude);
      console.log(longitude);
      
 
      let model = document.createElement('a-entity');
      model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-     model.setAttribute('gltf-model', '../assets/trout/scene.gltf');
+     model.setAttribute('gltf-model', `../assets/${testje}/scene.gltf`);
      model.setAttribute('rotation', '0 180 0');
      model.setAttribute('animation-mixer', '');
      model.setAttribute('scale', '1 1 1');
