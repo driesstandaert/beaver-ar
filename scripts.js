650e82e8965e9c447acb067d51deba2ce36e607b
driesstandaert <driesstandaert@gmail.com>
2020-08-11 14:11:55 +0100

    Change master index

--- scripts.js
+++ scripts.js
@@ -1,88 +1,66 @@
 window.onload = () => {
-  const button = document.querySelector('button[data-action="change"]');
-  button.innerText = '';
-
   let places = staticLoadPlaces();
   renderPlaces(places);
+  console.log(places);
 };
 
 function staticLoadPlaces() {
   return [
-      {
-          name: 'creatures',
-          location: {
-              lat: 51.4779701, // Paul Lat
-              lng: -0.0849297, // Paul Lng
-          },
+    {
+      name: 'beaver',
+      url: './assets/beaver/beaver.gltf',
+      scale: '0.02 0.02 0.02',
+      location: {
+        // lat: 51.622540,
+        // lng: -0.177321,
+        lat: 51.478025,
+        lng: -0.084966
       },
+    },
+    {
+      name: 'trout',
+      url: './assets/trout/scene.gltf',
+      scale: '0.5 0.5 0.5',
+      location: {
+        lat: 51.4779701, // Paul Lat
+        lng: -0.0849297, // Paul Lng
+      },
+    },
+    {
+      name: 'mouse',
+      url: './assets/mouse/scene.gltf',
+      scale: '0.01 0.01 0.01',
+      location: {
+        lat: 51.477890,
+        lng: -0.085055,
+      },
+    },
   ];
 }
-
-var models = [
-  {
-    url: './assets/beaver/beaver.gltf',
-    scale: '0.02 0.02 0.02',
-    rotation: '0 250 0',
-    position: '3 -1 0',
-    info: 'Beaver'
-  },
-  {
-    url: './assets/trout/scene.gltf',
-    scale: '0.5 0.5 0.5',
-    rotation: '0 180 0',
-    position: '2 -1 0',
-    info: 'Trout'
-  },
-  {
-    url: './assets/magnemite/scene.gltf',
-    scale: '0.4 0.4 0.4',
-    rotation: '0 180 0',
-    position: '2 1 0',
-    info: 'Look up'
-  }
-];
-
-var modelIndex = 0;
-var setModel = function (model, entity) {
-  if (model.scale) {
-      entity.setAttribute('scale', model.scale);
-  }
-
-  if (model.rotation) {
-      entity.setAttribute('rotation', model.rotation);
-  }
-
-  if (model.position) {
-      entity.setAttribute('position', model.position);
-  }
-
-  entity.setAttribute('gltf-model', model.url);
-
-  const div = document.querySelector('.instructions');
-  div.innerText = model.info;
-};
 
 function renderPlaces(places) {
   let scene = document.querySelector('a-scene');
 
   places.forEach((place) => {
-      let latitude = place.location.lat;
-      let longitude = place.location.lng;
+    console.log(place.name);
+    console.log(place.location.lat);
+    console.log(place.location.lng);
 
-      let model = document.createElement('a-entity');
-      model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    let model = document.createElement('a-entity');
+    model.setAttribute('gps-entity-place', `latitude: ${place.location.lat}; longitude: ${place.location.lng};`);
+    model.setAttribute('gltf-model', `${place.url}`);
+    model.setAttribute('rotation', '0 0 0');
+    model.setAttribute('animation-mixer', '');
+    model.setAttribute('scale', place.scale);
 
-      setModel(models[modelIndex], model);
+    model.addEventListener('loaded', () => {
+      window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'));
+      console.log(model);
+    });
 
-      model.setAttribute('animation-mixer', '');
+    scene.appendChild(model);
 
-      document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-          const entity = document.querySelector('[gps-entity-place]');
-          modelIndex++;
-          const newIndex = modelIndex % models.length;
-          setModel(models[newIndex], entity);
-      });
-
-      scene.appendChild(model);
+    // const distanceMsg = document.querySelector('[gps-entity-place]').getAttribute('distanceMsg');
+    // console.log(model);   // "890 meters"
   });
 }
