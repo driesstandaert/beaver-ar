37b7f03c379c54fa23d2e8f874f33302d3e56e16
driesstandaert <driesstandaert@gmail.com>
2020-08-17 14:14:28 +0100

    Add loading + marker lost and found

---
+++ markerless/scripts.js
@@ -0,0 +1,62 @@
+/* global AFRAME, THREE */
+
+/**
+ * Player for animation clips. Intended to be compatible with any model format that supports
+ * skeletal or morph animations.
+ */
+AFRAME.registerComponent('play-all-model-animations', {
+  init: function () {
+    this.model = null;
+    this.mixer = null;
+
+    var model = this.el.getObject3D('mesh');
+    if (model) {
+      this.load(model);
+    } else {
+      this.el.addEventListener('model-loaded', function (e) {
+        this.load(e.detail.model);
+      }.bind(this));
+    }
+  },
+
+  load: function (model) {
+    this.model = model;
+    this.mixer = new THREE.AnimationMixer(model);
+    this.model.animations.forEach(animation => {
+      this.mixer.clipAction(animation, model).play();
+    });
+  },
+
+  tick: function (t, dt) {
+    if (this.mixer && !isNaN(dt)) {
+      this.mixer.update(dt / 1000);
+    }
+  }
+});
+
+AFRAME.registerComponent('sun-position-setter', {
+  init: function () {
+    var skyEl = this.el;
+    var orbitEl = this.el.sceneEl.querySelector('#orbit');
+
+    orbitEl.addEventListener('componentchanged', function changeSun (evt) {
+      var sunPosition;
+      var phi;
+      var theta;
+
+      if (evt.detail.name !== 'rotation') { return; }
+
+      sunPosition = orbitEl.getAttribute('rotation');
+
+      if(sunPosition === null) { return; }
+
+      theta = Math.PI * (- 0.5);
+      phi = 2 * Math.PI * (sunPosition.y / 360 - 0.5);
+      skyEl.setAttribute('material', 'sunPosition', {
+        x: Math.cos(phi),
+        y: Math.sin(phi) * Math.sin(theta),
+        z: -1
+      });
+    });
+  }
+});
